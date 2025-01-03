0cb93da4fdebfbc741dbd40b30dbbd381a2cd973
Bastianmoral <bastihc@gmail.com>
2025-01-03 14:01:34 -0300

    fixing issue whit typo

--- src/routes/cancerDetails.js
+++ src/routes/cancerDetails.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
-const CancerDetails = require("../models/CancerDetails");
+const CancerDetails = require("../models/cancerDetails");
 
 // Middleware para manejar errores de validación
 const validateRequest = (req, res, next) => {
